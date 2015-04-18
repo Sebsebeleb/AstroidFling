7caa3c082a5acbbede6c90cd2e7f175fecd68817
NameTagz <flokander@gmail.com>
2015-04-18 23:41:23 +0200

    lok in da haoz

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour {
+
+	private Vector2 directionPlanet;
+	private Vector2 directionSpeed;
+
+	private Rigidbody2D rig;
+
+	private bool InOrbit;
+	
+	void Start () {
+
+		rig = GetComponent<Rigidbody2D>();
+
+	}
+
+	void Update () {
+
+		if (InOrbit) ApplyGravity();
+		else {
+
+			Vector3 mPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			Vector2 directionMouse = new Vector2(transform.position.x - mPos.x, transform.position.y - mPos.y).normalized;
+			float speedFactor = Vector2.Distance (transform.position, mPos) * 2f;
+
+			if (Input.GetMouseButtonUp (0)) {
+				directionSpeed = directionMouse * speedFactor;
+				StartOrbit();
+			}
+		}
+
+	}
+
+	void StartOrbit () {
+		rig.AddForce (directionSpeed, ForceMode2D.Impulse);
+		InOrbit = true;
+	}
+
+	void ApplyGravity () {
+		directionPlanet = -transform.position;
+		directionPlanet.Normalize ();
+		directionPlanet *= (20-Vector2.Distance (new Vector2(), transform.position));
+
+		rig.AddForce (directionPlanet);
+	}
+}
---
+++ Assets/Scripts/AsteroidManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidManager : MonoBehaviour {
+
+	public GameObject AsteroidPrefab;
+	private bool Busy;
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+
+		Vector3 asteroidPosition = new Vector3();
+
+		if (!Busy && Input.GetMouseButtonDown(0)) {
+			Vector3 mPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			asteroidPosition = mPos.normalized * 100f;
+		}
+		else if (!Busy && Input.GetMouseButton (0)) {
+			GameObject obj = Instantiate (AsteroidPrefab, asteroidPosition, Quaternion.identity) as GameObject;
+			obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y, 0f);
+			Busy = true;
+		}
+		else if (Input.GetMouseButtonUp(0)) {
+			Busy = false;
+		}
+	
+	}
+}
