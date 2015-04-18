3a3e5277ddb4c91e989190cf03b266681e64ed26
Jeesoka <jlostgaming@hotmail.com>
2015-04-18 23:19:09 +0200

    Forgot meta file project settings

---
+++ Assets/Scripts/EarthManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthManager : MonoBehaviour 
+{
+
+    public int lives;
+    public bool noLives { get { return lives <= 0; } set { } }
+
+	void Start () 
+    {
+
+        lives = 10;
+	
+	}
+	
+	void Update ()
+    {
+
+        if (noLives)
+        {
+            /*Death*/
+            Application.LoadLevel(Application.loadedLevel);
+        }
+	
+	}
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "enemy1")
+        {
+            lives--;
+            Destroy(col.gameObject);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour 
+{
+
+    private Transform target;
+
+    public float eSpeed;
+
+	void Start () 
+    {
+
+        eSpeed = 1f;
+        target = GameObject.FindGameObjectWithTag("earth").transform;
+	
+	}
+	
+	void Update () 
+    {
+
+        if (Vector3.Distance(transform.position, target.position) > 0.1f)
+        {
+            float a = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x);
+            Vector3 rot = transform.eulerAngles;
+            rot.z = 90 + a * 180 / Mathf.PI;
+            transform.eulerAngles = rot;
+            Vector3 pos = transform.position;
+            pos.x += eSpeed * Mathf.Cos(a) * Time.deltaTime;
+            pos.y += eSpeed * Mathf.Sin(a) * Time.deltaTime;
+            transform.position = pos;
+        }
+	
+	}
+
+}
