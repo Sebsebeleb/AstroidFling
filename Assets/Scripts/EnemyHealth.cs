2d36f40b59a068fee9a923e1f13195b9abdc9930
Sebastian Vråle <seb@terminator.net>
2015-04-19 03:27:25 +0200

    Bzzt

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour
+{
+
+    public float Health;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage(float damage)
+    {
+        Health -= damage;
+        if (Health <= 0) {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/EnemySunDamager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class EnemySunDamager : MonoBehaviour
+{
+    private Transform sun;
+    private EnemyHealth health;
+
+    private bool isRayBlocked = false;
+    private float sunLightRadius = 10;
+
+    private void Start()
+    {
+        sun = GameObject.FindWithTag("Sun").transform;
+        health = GetComponent<EnemyHealth>();
+    }
+
+    private void Update()
+    {
+        CheckInShadow();
+        CheckInRadius();
+    }
+
+    /// <summary>
+    /// Are we close enough to the sun? if not take damage
+    /// </summary>
+    private void CheckInRadius()
+    {
+        if (Vector2.Distance(transform.position, sun.position) > sunLightRadius) {
+            health.TakeDamage(Time.deltaTime);
+        }
+    }
+
+    /// <summary>
+    /// Are we in a shadow from the sun?
+    /// </summary>
+    private void CheckInShadow()
+    {
+        RaycastHit2D rayHit = Physics2D.Raycast(transform.position, (sun.position - transform.position));
+        isRayBlocked = false;
+
+        // We hit something
+        //if (rayHit.distance < Vector2.Distance(transform.position, sun.position) - 0.05f)
+        if (rayHit.collider != null)
+        {
+            Debug.Log(rayHit.distance);
+            health.TakeDamage(Time.deltaTime);
+            isRayBlocked = true;
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        if (sun == null)
+        {
+            return;
+        }
+
+        if (isRayBlocked)
+        {
+            Gizmos.color = Color.red;
+        }
+        else
+        {
+            Gizmos.color = Color.green;
+        }
+        Gizmos.DrawLine(transform.position, sun.position);
+    }
+}
