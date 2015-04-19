936879f0282e31441175676e54013a702874e585
NameTagz <flokander@gmail.com>
2015-04-19 21:43:22 +0200

    yowsa

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -18,10 +18,14 @@ public class CameraControl : MonoBehaviour {
 			pos.x = Random.Range (-r, r);
 			pos.y = Random.Range (-r, r);
 			pos.z = -10f;
-			transform.position = pos;
+			transform.position = Vector3.Lerp (transform.position, pos, 0.7f);
 		}
 		else {
 			transform.position = new Vector3(0f, 0f, -10f);
+		}
+
+		if (Input.GetKey(KeyCode.Space)) {
+			StartShake(0.7f, 2f);
 		}
 	
 	}
